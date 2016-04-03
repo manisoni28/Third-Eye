bec6028edf632356dd023352e4016b59e4010b94
manisoni28 <manisoni28@gmail.com>
2016-04-03 06:44:41 +0530

    Added sms constants and reciever to the project

---
+++ app/src/main/java/com/Appic/ThirdEye/ComposeSmsActivity.java
@@ -0,0 +1,8 @@
+package com.Appic.ThirdEye;
+
+import android.app.Activity;
+
+public class ComposeSmsActivity extends Activity
+	{
+    // TODO: Implement SMS/MMS compose functions and need to respond ACTION_SENDTO Intnet
+	}
